3ff1a1ada921f723095461ad8606961bf5bea908
Asen Chochev <asen.qkiqt145@gmail.com>
2025-06-07 18:40:26 +0300

    Import logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../../src/assets/images/logo.png'
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <img src={logo} className='w-[80px] md:w-[115px] object-cover' alt="logo" />
     </div>
   )
 }
